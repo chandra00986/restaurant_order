37b3ebab620da06b26ac71410da00b4c8781dbee
Chandra <62059587+Chandra-1@users.noreply.github.com>
2022-03-07 12:00:34 +0700

    first commit

---
+++ components/Categories.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { View, Text, Image, Platform, ScrollView } from "react-native";
+
+const items = [
+  {
+    image: require("../assets/images/shopping-bag.png"),
+    text: "Pick-up",
+  },
+  {
+    image: require("../assets/images/soft-drink.png"),
+    text: "Soft Drinks",
+  },
+  {
+    image: require("../assets/images/bread.png"),
+    text: "Bakery Items",
+  },
+  {
+    image: require("../assets/images/fast-food.png"),
+    text: "Fast Foods",
+  },
+  {
+    image: require("../assets/images/deals.png"),
+    text: "Deals",
+  },
+  {
+    image: require("../assets/images/coffee.png"),
+    text: "Coffee & Tea",
+  },
+  {
+    image: require("../assets/images/desserts.png"),
+    text: "Desserts",
+  },
+];
+
+export default function Categories() {
+  return (
+    <View style={{ marginTop: 5, backgroundColor: "#fff", paddingLeft: 20 }}>
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        {/* {Loop start here} */}
+        {items.map((item, index) => (
+          <View key={index} style={{ alignItems: "center", marginRight: 30 }}>
+            <Image
+              source={item.image}
+              style={{ width: 50, height: 40, resizeMode: "contain" }}
+            />
+            <Text
+              style={{
+                fontSize: 13,
+                fontWeight: "bold",
+              }}
+            >
+              {item.text}
+            </Text>
+          </View>
+        ))}
+        {/* {Loop end here} */}
+      </ScrollView>
+    </View>
+  );
+}
